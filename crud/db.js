fee2ded32dd7a9bf0c521997a94a2bd2bcb32df2
Cristian Bevilaqua <104402733+Kemeker@users.noreply.github.com>
2023-10-09 14:42:29 -0300

    create db com parametros dos alunos

---
+++ crud/db.js
@@ -0,0 +1,31 @@
+let alunos = [
+    {
+        id: 1,
+        nome: "Cristian",
+        email: "cristian@teste.com"
+        senha: "123",
+    },
+    {
+        id: 2,
+        nome: "Elisa"
+        email: "elisa@teste.com"
+        senha: "4321",
+
+    },
+    {
+        id: 3,
+        nome: "Vicente"
+        email: "vicente@teste.com"
+        senha: "12345"
+    },
+    {
+        id: 4,
+        nome: "Ana Beatriz"
+        email: "anabeatriz@teste.com"
+        senha: "890"
+    },
+
+]
+module.exports = {
+    alunos,
+}
